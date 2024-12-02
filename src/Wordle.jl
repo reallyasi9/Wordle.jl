7e7415501f13962a4443ef2a168edb37f727126a
--get <reallyasi9@users.noreply.github.com>
2024-12-02 18:11:10 -0500

    Add basic word functionality

--- src/Wordle.jl
+++ src/Wordle.jl
@@ -1,5 +1,214 @@
 module Wordle
 
-# Write your package code here.
+using Crayons
+
+export Word, WordStatus, GuessResult, Possibilities
+export letterstatus, HERE, SOMEWHERE, NOWHERE
+export statuscrayon, statusindex, guess
+
+struct Word
+    letters::NTuple{5,Char}
+end
+
+function Word(word::AbstractString)
+    letters = NTuple{5,Char}(lowercase(word))
+    return Word(letters)
+end
+
+function Base.show(io::IO, mime::MIME"text/plain", word::Word)
+    show(io, mime, join(word.letters))
+end
+
+function Base.getindex(word::Word, i)
+    return word.letters[i]
+end
+
+function Base.lastindex(::Word)
+    return 5
+end
+
+function Base.keys(word::Word)
+    return keys(word.letters)
+end
+
+@enum LetterStatus NOWHERE=1 SOMEWHERE=2 HERE=3
+
+function letterstatus(c::AbstractChar)
+    if c == '-'
+        return NOWHERE
+    elseif c == '?'
+        return SOMEWHERE
+    elseif c == '+'
+        return HERE
+    else
+        throw(ArgumentError("letter status '$c' not recognized"))
+    end
+end
+
+function statuscrayon(s::LetterStatus)
+    if s == NOWHERE
+        return Crayon(reset=true)
+    elseif s == SOMEWHERE
+        return Crayon(reset=true, foreground=:yellow, italics=true)
+    elseif s == HERE
+        return Crayon(reset=true, foreground=:green, underline=true)
+    else
+        return Crayon(reset=true, background=:red)
+    end
+end
+
+struct WordStatus
+    status::NTuple{5,LetterStatus}
+end
+
+function WordStatus(s::AbstractString)
+    st = NTuple{5,LetterStatus}(letterstatus.(collect(s)))
+    return WordStatus(st)
+end
+
+function Base.getindex(status::WordStatus, i)
+    return status.status[i]
+end
+
+function Base.lastindex(::WordStatus)
+    return 5
+end
+
+function Base.keys(status::WordStatus)
+    return keys(status.status)
+end
+
+function statusindex(status::WordStatus)
+    idx = 0
+    pow = 1
+    for i in eachindex(status)
+        idx += (Int(status[i]) - 1) * pow
+        pow *= 3
+    end
+    return idx + 1
+end
+
+struct GuessResult
+    word::Word
+    status::WordStatus
+end
+
+function GuessResult(word::AbstractString, status::AbstractString)
+    return GuessResult(Word(word), WordStatus(status))
+end
+
+function Base.getindex(result::GuessResult, i)
+    return (result.word[i], result.status[i])
+end
+
+function Base.keys(result::GuessResult)
+    return keys(result.word)
+end
+
+function Base.lastindex(::GuessResult)
+    return 5
+end
+
+function Base.show(io::IO, ::MIME"text/plain", guess::GuessResult)
+    for l in eachindex(guess)
+        print(io, statuscrayon(guess.status[l]))
+        print(io, guess.word[l])
+    end
+end
+
+function guess(word::Word, solution::Word)
+    # tricky: repeated letters
+    # a HERE letter overrides a SOMEWHERE letter, so hello:world -> ---+?, NOT --?+?
+    # the first SOMEWHERE letter overrides the second, so llama:world -> ?----, NOT ??---
+    # two popable lists might be the only reasonable way to do this
+    wordlist = collect(word.letters)
+    solnlist = collect(solution.letters)
+    
+    # here letters override all
+    heres = findall(wordlist .== solnlist)
+    wordlist[heres] .= '+'
+    solnlist[heres] .= '+'
+
+    # somewhere letters are ordered from left to right
+    for i in eachindex(wordlist)
+        c = wordlist[i]
+        if c == '+'
+            continue
+        end
+        j = findfirst(==(c), solnlist)
+        if !isnothing(j)
+            wordlist[i] = '?'
+            solnlist[j] = '?'
+        end
+    end
+
+    # everything else is a miss
+    for i in eachindex(wordlist)
+        c = wordlist[i]
+        if c != '+' && c != '?'
+            wordlist[i] = '-'
+        end
+    end
+
+    return GuessResult(word, WordStatus(String(wordlist)))
+end
+
+struct Possibilities
+    letters::NTuple{5,Set{Char}}
+    minima::NTuple{26,Int8}
+    maxima::NTuple{26,Int8}
+
+    function Possibilities(guesses::Vector{GuessResult})
+        # multiple letters is tricky
+        possible = (Set('a':'z'), Set('a':'z'), Set('a':'z'), Set('a':'z'), Set('a':'z'))
+        minima = zeros(Int8, 26)
+        maxima = ones(Int8, 26) .* Int8(5)
+
+        for gr in guesses
+            localmins = zeros(Int8, 26)
+            localmaxs = ones(Int8, 26) .* Int8(5)
+            for i in eachindex(gr)
+                c = gr.word[i]
+                idx = c - 'a' + 1
+                if gr.status[i] == HERE
+                    intersect!(possible[i], c)
+                    localmins[idx] += 1
+                    localmaxs[idx] += 1 # trick to make sure we catch a HERE or SOMEWHERE after a NOWHERE
+                elseif gr.status[i] == SOMEWHERE
+                    setdiff!(possible[i], c)
+                    localmins[idx] += 1
+                    localmaxs[idx] += 1 # trick to make sure we catch a HERE or SOMEWHERE after a NOWHERE
+                else
+                    setdiff!(possible[i], c)
+                    localmaxs[idx] = localmins[idx]
+                end
+            end
+            minima = max.(minima, localmins)
+            maxima = min.(maxima, localmaxs)
+        end
+        return new(possible, NTuple{26,Int8}(minima), NTuple{26,Int8}(maxima))
+    end
+end
+
+function Base.isvalid(possible::Possibilities, word::Word)
+    counts = zeros(Int8, 26)
+    for i in eachindex(word)
+        c = word[i]
+        if c ∉ possible.letters[i]
+            return false
+        end
+        idx = c - 'a' + 1
+        counts[idx] += 1
+        if counts[idx] > possible.maxima[idx]
+            return false
+        end
+    end
+    for i in eachindex(counts)
+        if counts[i] < possible.minima[i]
+            return false
+        end
+    end
+    return true
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,119 @@
 using Wordle
 using Test
+using TestItems
+using TestItemRunner
 
-@testset "Wordle.jl" begin
-    # Write your tests here.
+
+@testitem "Word Constructor" begin
+    w1 = Word("hello")
+    @test w1.letters == ('h', 'e', 'l', 'l', 'o')
+    
+    w2 = Word("HeLlO")
+    @test w2.letters == w1.letters
+
+    w3 = Word("hello!") # truncates
+    @test w3.letters == w1.letters
+
+    @test_throws ArgumentError Word("hi")
 end
+
+@testitem "Word getindex" begin
+    w1 = Word("hello")
+    @test w1[1] == 'h'
+    @test w1[2:end] == ('e', 'l', 'l', 'o')
+    @test_throws BoundsError w1[6]
+end
+
+@testitem "Word iterate" begin
+    let
+        w1 = Word("hello")
+        x = 1
+        for i in eachindex(w1)
+            @test w1[i] == w1[x]
+            x += 1
+        end
+    end
+end
+
+@testitem "letterstatus" begin
+    @test letterstatus('-') == NOWHERE
+    @test letterstatus('?') == SOMEWHERE
+    @test letterstatus('+') == HERE
+    @test_throws ArgumentError letterstatus('x')
+end
+
+@testitem "statuscrayon" begin
+    using Crayons
+    @test statuscrayon(letterstatus('-')) == Crayon(reset=true)
+    @test statuscrayon(letterstatus('?')) == Crayon(foreground=:yellow, italics=true)
+    @test statuscrayon(letterstatus('+')) == Crayon(foreground=:green, underline=true)
+    @test_throws ArgumentError statuscrayon(letterstatus('x'))
+end
+
+@testitem "WordStatus Constructor" begin
+    ws1 = WordStatus("+-?+-")
+    ws2 = WordStatus((HERE, NOWHERE, SOMEWHERE, HERE, NOWHERE))
+    @test ws1.status == ws2.status
+
+    ws3 = WordStatus("+-?+-?") # truncates
+    @test ws3.status == ws1.status
+
+    @test_throws ArgumentError WordStatus("+-")
+    @test_throws ArgumentError WordStatus("+-x+-")
+end
+
+@testitem "WordStatus getindex" begin
+    ws1 = WordStatus("+-?+-")
+    @test ws1[1] == HERE
+    @test ws1[2:end] == (NOWHERE, SOMEWHERE, HERE, NOWHERE)
+    @test_throws BoundsError ws1[6]
+end
+
+@testitem "WordStatus iterate" begin
+    let
+        ws1 = WordStatus("+-?+-")
+        x = 1
+        for i in eachindex(ws1)
+            @test ws1[i] == ws1[x]
+            x += 1
+        end
+    end
+end
+
+@testitem "statusindex" begin
+    @test statusindex(WordStatus("-----")) == 1
+    @test statusindex(WordStatus("+++++")) == 3^5
+    @test statusindex(WordStatus("+-?+-")) == 2 * 3^0 + 0 * 3^1 + 1 * 3^2 + 2 * 3^3 + 0 * 3^4 + 1
+end
+
+@testitem "GuessResult Constructor" begin
+    gr1 = GuessResult(Word("hello"), WordStatus("+-?+-"))
+    gr2 = GuessResult("hello", "+-?+-")
+    @test gr1.word == gr2.word
+    @test gr1.status == gr2.status
+
+    gr3 = GuessResult("HeLlO!", "+-?+-?") # truncates and normalizes
+    @test gr3.word == gr1.word
+    @test gr3.status == gr1.status
+
+
+    @test_throws ArgumentError GuessResult("hello", "+-")
+    @test_throws ArgumentError GuessResult("hi", "+-?+-")
+    @test_throws ArgumentError GuessResult("hello", "+-x+-")
+end
+
+@testitem "guess" begin
+    @test guess(Word("hello"), Word("world")) == GuessResult("hello", "---+?")
+    @test guess(Word("llama"), Word("world")) == GuessResult("llama", "?----")
+    @test guess(Word("xxxyy"), Word("zxzzx")) == GuessResult("xxxyy", "?+---")
+end
+
+@testitem "Possibilities isvalid" begin
+    gr1 = guess(Word("hello"), Word("world"))
+    gr2 = guess(Word("llama"), Word("world"))
+    guesses = [gr1, gr2]
+    p = Possibilities(guesses)
+    
+end
+
+@run_package_tests
