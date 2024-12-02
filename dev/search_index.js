52c96aa737c1460c1c0238da0f5c0d813d4f136e
Documenter.jl <documenter@juliadocs.github.io>
2024-12-02 23:15:03 +0000

    build based on 7e74155

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Wordle","category":"page"},{"location":"#Wordle","page":"Home","title":"Wordle","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Wordle.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Wordle]","category":"page"}]
+}
